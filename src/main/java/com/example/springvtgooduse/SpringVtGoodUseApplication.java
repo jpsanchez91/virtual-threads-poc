0dee517b1e5230e7c92a95fdd12c65f5b3fe96a1
Joao Paulo Sanchez <joaopaulosanchez@192.168.1.21>
2024-11-13 15:31:29 -0300

    first commit

---
+++ src/main/java/com/example/springvtgooduse/SpringVtGoodUseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springvtgooduse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVtGoodUseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVtGoodUseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springvtgooduse/SpringVtGoodUseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springvtgooduse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVtGoodUseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
